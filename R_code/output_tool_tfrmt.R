f74c5ae0ff1cfe1054e1c63158be24ff6bdd7b16
Malan <64360731+malanbos@users.noreply.github.com>
2024-01-10 10:25:44 +0200

    Add files via upload

---
+++ R_code/output_tool_tfrmt.R
@@ -0,0 +1,13 @@
+# title:    tfrmt_output_tool
+# purpose:  Reads in analysis results dataset, output creation specs, and produces outputs using tfrmt package
+# Author:   Malan Bosman
+# Date:     28NOV2023
+
+# libraries ---------------------------------------------------------------
+library(tidyverse)
+library(readxl)
+library(officer)
+
+
+# get ARD -----------------------------------------------------------------
+
